bf9bf8fc1042d88d7fda741c0d58267e78898a02
Umair Khan <umairkhan.cis@gmail.com>
2020-06-28 00:36:11 +0400

    add stack and queue adt and tests

---
+++ stack.py
@@ -0,0 +1,18 @@
+class Stack(object):
+    def __init__(self):
+        print("Todo")
+
+    def push(self, data):
+        print("Todo")
+
+    def pop(self):
+        print("Todo")
+
+    def top(self):
+        print("Todo")
+
+    def isEmpty(self):
+        print("Todo")
+
+    def size(self):
+        print("Todo")
