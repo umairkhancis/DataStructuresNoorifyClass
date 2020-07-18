5f193f17301e294ad09b43c73c2d560c7c484149
Asadullahkhan <asadmuhammad427@gmail.com>
2020-07-18 19:32:47 +0500

    added request changes

--- asad_recursion_homework/recursion_assignment.js
+++ asad_recursion_homework/recursion_assignment.js
@@ -31,7 +31,7 @@ print_0toN=(n)=>{
 }
 
 
-print_Negative_N_to_N=(n)=>{ //2
+print_Negative_N_to_N=(n)=>{ 
     if(n < -N){
         return
     }
@@ -42,22 +42,34 @@ print_Negative_N_to_N=(n)=>{ //2
     }
 }
 
+slice_arr=(_array,start_index, end_index = _array.length)=>{
+    let i = start_index
+    let j = 0
+    let _arr =[]
+    while(i !== end_index){
+     _arr[j] = _array[i]   
+     ++j
+     ++i
+    }
+    return _arr
+}
+
 printArr=(arr)=>{
     if(arr.length === 0){
         return
     }    
-    printArr(arr.slice(0,arr.length-1))
-    arra.push(arr[arr.length-1])
+    printArr(slice_arr(arr, 0 ,arr.length-1))
     console.log(arr[arr.length-1])
 }
+
 
 printArrInReverse=(arr)=>{
     if(arr.length === 0){
         return
     }    
-    arra.push(arr[arr.length-1])
+    
     console.log(arr[arr.length-1])
-    printArrInReverse(arr.slice(0,arr.length-1))   
+    printArrInReverse(slice_arr(arr, 0, arr.length-1))   
 }
 
 printSumArr = (arr)=>{
@@ -65,39 +77,37 @@ printSumArr = (arr)=>{
         return 0
     }    
     else{
-        result = arr[0] + printSumArr(arr.slice(1)) // add all the items of arr except the first one and after that add the first element 
+        result = arr[0] + printSumArr(slice_arr(arr, 1)) // add all the items of arr except the first one and after that add the first element 
         return result
     }
 }
 
 isPalindrome=(str) =>{
     // base case #1
-    if(str.length === 0 || str.length === 1 || (str.length === 2 && str[0] == str.slice(-1))){
+    if(str.length === 0 || str.length === 1 || (str.length === 2 && str[0] == slice_arr(str, -1))){
      return true;    
     }
     // base case #2
-    if (str[0] !== str.slice(-1)){
+    if (str[0] !== slice_arr(str, -1)){
      return false;   
     }
     // recursive case
-    return isPalindrome(str.slice(1, -1));     
+    return isPalindrome(slice_arr(str, 1, -1));     
 }
 
 printStringInReverse= (str)=>{
     if(str.length === 0){
         return
     }
-    string.push(str[str.length-1])
     console.log(str[str.length-1])
-    printStringInReverse(str.slice(0,str.length-1))    
+    printStringInReverse(slice_arr(str, 0, str.length-1))    
 }
 
 
 // tests
 
-// let string = []
+
 // printStringInReverse("asad")
-// assert.ok(string,"dasa")
 
 
 
@@ -118,15 +128,13 @@ printStringInReverse= (str)=>{
 
 
 
-// let arra =[]
-// let array =[1,2,3]
+let array =[1,2,3]
 
 // printArrInReverse(array)
-// assert.ok(arra,[3,2,1])
 
 
 // printArr(array)
-// assert.ok(arra ,[1,2,3])
+
 
 
 // assert.equal(printSumArr(array),6)
@@ -146,13 +154,11 @@ printStringInReverse= (str)=>{
 
 
 
-
-
-assert.equal(isPalindrome("asffsa"),true)
-assert.equal(isPalindrome("asfrsa"),false)
-assert.equal(isPalindrome(""),true)
-assert.equal(isPalindrome("a"),true)
-assert.equal(isPalindrome("asad"),false)
+// assert.equal(isPalindrome("asffsa"),true)
+// assert.equal(isPalindrome("asfrsa"),false)
+// assert.equal(isPalindrome(""),true)
+// assert.equal(isPalindrome("a"),true)
+// assert.equal(isPalindrome("asad"),false)
 
 
 
@@ -160,11 +166,11 @@ assert.equal(isPalindrome("asad"),false)
 
 
 
-var linkedlist= new LinkedList()
-linkedlist.add_back(1) 
-linkedlist.add_back(2)
-linkedlist.add_back("3")
-linkedlist.add_back(4)
+// var linkedlist= new LinkedList()
+// linkedlist.add_back(1) 
+// linkedlist.add_back(2)
+// linkedlist.add_back("3")
+// linkedlist.add_back(4)
 
 // printLinkedList(linkedlist.head)
-printLinkedListInReverse(linkedlist.head)
+// printLinkedListInReverse(linkedlist.head)
